3f954a669a4f48d67a956a7f8c425be4ba3f4630
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-24 15:41:41 -0400

    Registration form mini-project from the textbook.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Chapter9_RegistrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ckBoxSwim = new System.Windows.Forms.CheckBox();
+            this.ckBoxSnorkel = new System.Windows.Forms.CheckBox();
+            this.ckBoxDive = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(102, 378);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(125, 27);
+            this.txtResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Lesson";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(47, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Check all that apply";
+            // 
+            // ckBoxSwim
+            // 
+            this.ckBoxSwim.AutoSize = true;
+            this.ckBoxSwim.Location = new System.Drawing.Point(47, 152);
+            this.ckBoxSwim.Name = "ckBoxSwim";
+            this.ckBoxSwim.Size = new System.Drawing.Size(105, 24);
+            this.ckBoxSwim.TabIndex = 6;
+            this.ckBoxSwim.Text = "Swim - $50";
+            this.ckBoxSwim.UseVisualStyleBackColor = true;
+            this.ckBoxSwim.CheckedChanged += new System.EventHandler(this.ComputeCost_CheckedChanged);
+            // 
+            // ckBoxSnorkel
+            // 
+            this.ckBoxSnorkel.AutoSize = true;
+            this.ckBoxSnorkel.Location = new System.Drawing.Point(47, 183);
+            this.ckBoxSnorkel.Name = "ckBoxSnorkel";
+            this.ckBoxSnorkel.Size = new System.Drawing.Size(118, 24);
+            this.ckBoxSnorkel.TabIndex = 7;
+            this.ckBoxSnorkel.Text = "Snorkel - $25";
+            this.ckBoxSnorkel.UseVisualStyleBackColor = true;
+            this.ckBoxSnorkel.CheckedChanged += new System.EventHandler(this.ComputeCost_CheckedChanged);
+            // 
+            // ckBoxDive
+            // 
+            this.ckBoxDive.AutoSize = true;
+            this.ckBoxDive.Location = new System.Drawing.Point(47, 214);
+            this.ckBoxDive.Name = "ckBoxDive";
+            this.ckBoxDive.Size = new System.Drawing.Size(107, 24);
+            this.ckBoxDive.TabIndex = 8;
+            this.ckBoxDive.Text = "Dive - $100";
+            this.ckBoxDive.UseVisualStyleBackColor = true;
+            this.ckBoxDive.CheckedChanged += new System.EventHandler(this.ComputeCost_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(214, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 171);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Skill Level";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(22, 123);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(96, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Advanced";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 82);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Intermediate";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Beginner";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ckBoxDive);
+            this.Controls.Add(this.ckBoxSnorkel);
+            this.Controls.Add(this.ckBoxSwim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox ckBoxSwim;
+        private System.Windows.Forms.CheckBox ckBoxSnorkel;
+        private System.Windows.Forms.CheckBox ckBoxDive;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter9_RegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComputeCost_CheckedChanged(object sender, EventArgs e) // Example 9-21, pg 520
+        {
+            /*
+             * A nested if-else statement here wouldn't work, because it is not known which check box
+             * will be selected first. 
+             */
+            decimal cost = 0;
+            if (this.ckBoxSwim.Checked)
+            {
+                cost += 50;
+            }
+            if (this.ckBoxSnorkel.Checked)
+            {
+                cost += 25;
+            }
+            if (this.ckBoxDive.Checked)
+            {
+                cost += 100;
+            }
+            this.txtResult.Text = cost.ToString("C");
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
